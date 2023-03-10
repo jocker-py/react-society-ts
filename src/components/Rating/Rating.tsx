64e3defe28e0565196ae86b1a2e5192ed61282da
jocker-py <buladzislau@gmail.com>
2023-03-10 20:33:37 +0300

    feat: add props selected for Star

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
-function Rating(props:{value:number}){
+function Rating(props:{value : number}){
   return (
     <div>
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-      <Star />
+      {
+        [...new Array(5)]
+          .map((star, idx) => {
+            return idx < props.value ?
+              <Star selected={true} /> :
+              <Star selected={false} />
+          })
+      }
     </div>)
 }
 
-function Star(){
-  return(<div>*</div>)
+function Star(props:{selected: boolean}){
+  return(<span>{props.selected ? <b>star </b> : 'star '}</span>)
 }
 
 export default Rating;
