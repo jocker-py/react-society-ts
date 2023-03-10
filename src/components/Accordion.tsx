4169f90ce6d67a84e342781340f05c263bfa4a67
jocker-py <buladzislau@gmail.com>
2023-03-10 19:42:53 +0300

    feat: add component Accordion

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AccordionTitle from "./AccordionTitle";
+import AccordionBody from "./AccordionBody";
+
+function Accordion(){
+  return(
+    <div>
+      <AccordionTitle />
+      <AccordionBody />
+    </div>
+  )
+}
+
+export default Accordion;
