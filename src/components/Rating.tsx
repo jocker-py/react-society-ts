db6eb1e011a5fbbaa8890a423e0f3e723d789d2e
jocker-py <buladzislau@gmail.com>
2023-03-10 19:43:03 +0300

    feat: add component Rating

---
+++ src/components/Rating.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Star from "./Star";
+
+function Rating(){
+  return (
+    <>
+    <Star />
+    <Star />
+    <Star />
+    <Star />
+    <Star />
+  </>)
+}
+
+export default Rating;
