e2c334cfa4f909eb173adc8fec2d3b2a9e2cc07a
jocker-py <buladzislau@gmail.com>
2023-04-09 20:21:31 +0300

    feat: add OnOff button

---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import s from "./OnOff.module.css";
+
+export const OnOff = () => {
+  const [on, setOn] = useState(false);
+  const styleOff = on ? `${s.button} ${s.off}` : `${s.button} ${s.off} ${s.active}`;
+  const styleOn = on ? `${s.button} ${s.on} ${s.active}` : `${s.button} ${s.on}`;
+  const labelOn = `${s.label} ${s.labelOn}`;
+  const labelOff = `${s.label} ${s.labelOff}`;
+  return (
+    <div className={s.block}>
+      <div className={styleOn} onClick={() => setOn(true)}>On</div>
+      <div className={styleOff} onClick={() => setOn(false)}>Off</div>
+      <div className={s.labelBox}>
+        <label className={on ? labelOn : labelOff}/>
+      </div>
+    </div>
+  );
+};
