99202e7709ba11b208128610adc65cd17435cf56
jocker-py <buladzislau@gmail.com>
2023-05-11 14:41:57 +0300

    feat: add React.memo for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Rating} from "./components/Rating/Rating";
-import {Accordion} from "./components/Accordion/Accordion";
-import {OnOff} from "./components/OnOff/OnOff";
-import {UncontrolledAccordion} from "./components/Accordion/UncontrolledAccordion";
-import {UncontrolledRating} from "./components/Rating/UncontrolledRating";
+import Rating from "./components/Rating/Rating";
+import Accordion from "./components/Accordion/Accordion";
+import OnOff from "./components/OnOff/OnOff";
+import UncontrolledAccordion from "./components/Accordion/UncontrolledAccordion";
+import UncontrolledRating from "./components/Rating/UncontrolledRating";
 
 
 const App = () => {
@@ -32,4 +32,4 @@ function AppTitle(props:AppTitleProps){
   return <h1>{props.title}</h1>
 }
 
-export default App;
+export default React.memo(App)
